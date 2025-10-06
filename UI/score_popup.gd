4717220859e66918794efe053859e7e967bad08c
DubiousDuck <oscar517730@gmail.com>
2025-10-06 10:43:36 -0700

    dynamic popup score size

--- UI/score_popup.gd
+++ UI/score_popup.gd
@@ -4,6 +4,8 @@ class_name ScorePopup
 
 const DURATION_SEC: int = 1.5
 
+const BASE_FONT_SIZE = 12
+
 var comment_list = ["Oh no!", "Nice", "Good!", "Beautiful!", "Fantastic!", "AMAZING!", "UNBELIEVABLE!"]
 
 @onready var label = $VBoxContainer/Label
@@ -14,7 +16,9 @@ func set_popup(score: int):
 		label.text = "+" + str(score)
 	else: label.text = "-" + str(abs(score))
 	
-	comment.text = comment_list[get_comment_index(score)]
+	var comment_index = get_comment_index(score)
+	comment.text = comment_list[comment_index]
+	set_font_size(BASE_FONT_SIZE * (comment_index + 1))
 	
 	show_and_fade()
 
@@ -34,3 +38,7 @@ func show_and_fade():
 	var a = get_tree().create_tween().set_parallel(true).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
 	a.tween_property(self, "global_position", global_position + Vector2.UP * 20, DURATION_SEC)
 	a.tween_property(self, "modulate:a", 0, DURATION_SEC)
+
+func set_font_size(font_size: int):
+	$VBoxContainer/Label.add_theme_font_size_override("font_size", font_size)
+	$VBoxContainer/Comment.add_theme_font_size_override("font_size", font_size)
