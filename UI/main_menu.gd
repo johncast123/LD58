7c3c11faa0a18e8628594ad632c97bd2cac31986
DubiousDuck <oscar517730@gmail.com>
2025-10-05 20:15:00 -0700

    main menu animation

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -3,8 +3,10 @@ extends Node2D
 @export var first_level: PackedScene
 
 func _ready():
+	AudioManager.play_bgm("Title")
 	Global.set_next_level(first_level)
 
 func _on_start_pressed():
 	if first_level:
+		AudioManager.play_sfx("ui_click")
 		Global.go_to_next_level()
