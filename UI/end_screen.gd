49ff8a4c1a2101d770317e66e9e355ee7d363c56
DubiousDuck <oscar517730@gmail.com>
2025-10-06 11:31:53 -0700

    Ending screen and visual guide update

---
+++ UI/end_screen.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@export_file("*.tscn") var start_level_path # use scene path to avoid cyclical dependency
+
+func _ready():
+	$CanvasLayer/VBoxContainer/Score.text = "Total Score: %06d" % Global.total_score
+	AudioManager.play_bgm("Level 4")
+	
+func _on_play_again_pressed():
+	AudioManager.play_sfx("ui_click")
+	Global.initialize_total_score()
+	if start_level_path:
+		get_tree().change_scene_to_file(start_level_path)
--- UI/visual_guide.gd
+++ UI/visual_guide.gd
@@ -3,4 +3,5 @@ extends CanvasLayer
 signal window_closed
 
 func _on_button_pressed():
+	AudioManager.play_sfx("ui_click")
 	emit_signal("window_closed")
